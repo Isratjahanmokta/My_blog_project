c724a93e78ba380d7501253352987652237cbfda
Israt <israt.mukta06@gmail.com>
2022-04-20 22:37:14 +0600

    init

--- My_blog_project/urls.py
+++ My_blog_project/urls.py
@@ -1,6 +1,9 @@
+from xml.dom.minidom import Document
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
 from . import views
+from django.contrib.staticfiles.urls import static, staticfiles_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -8,3 +11,6 @@ urlpatterns = [
     path('blog/',include('App_blog.urls')),
     path('', views.Index, name='Index'),
 ]
+
+urlpatterns+= staticfiles_urlpatterns()
+urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_root)
